445865e43f76a2554879168d0fbea924da904f81
Colin Neville <thecowlord@gmail.com>
2011-05-20 18:36:43 -0400

    Structuring

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,14 @@
 class HomeController < ApplicationController
   def index
+	@title = ""
   end
 
+  def contact
+	@title = "Contact"
+  end
+  
+  def about
+	@title = "About"
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Lft::Application.routes.draw do
   get "home/index"
+  get "home/contact"
+  get "home/about"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
